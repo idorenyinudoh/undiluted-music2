dfb6696e2b668e4bb58b984b50fed0745327191a
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-06-01 23:25:15 +0100

    Basic look of search part of header.js

---
+++ behaviour/header.js
@@ -0,0 +1,11 @@
+var search = document.getElementById('search-button');
+var header = document.getElementById('header');
+var form = document.getElementById('form');
+var clear = document.getElementById('clear-button');
+
+var toggle = () => {
+
+};
+
+search.addEventListener('click', toggle);
+clear.addEventListener('click', toggle);
