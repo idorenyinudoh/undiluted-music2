b55358a6f15480e38cfdea79e5067194b72963ae
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-09-13 14:32:19 +0100

    render home page

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const home = require('./routes/index');
 const app = express();
 const port = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.use(express.static('./public'));
+
+app.use('/', home);
 
 app.listen(port, () => {
     console.log(`Example listening at http://localhost:${port}`);
