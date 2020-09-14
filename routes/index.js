b3ec5d96830365236280bf519ff2243d85c679b6
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-09-14 06:36:14 +0100

    render genre page

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,43 @@ router.get('/eps', (req, res) => {
     });
 });
 
+const genres = {
+    'afrobeats': 'Afrobeats',
+    'afro-fusion': 'Afro-Fusion',
+    'afro-house': 'Afro-House',
+    'afro-pop': 'Afro-Pop',
+    'alternative-r&b': 'Alternative R&B',
+    'alternative-rock': 'Alternative Rock',
+    'banku': 'Banku',
+    'blues': 'Blues',
+    'contemporary-r&b': 'Contemporary R&B',
+    'dancehall': 'Dancehall',
+    'electronic': 'Electronic',
+    'folk': 'Folk',
+    'fuji': 'Fuji',
+    'gospel': 'Gospel',
+    'highlife': 'Highlife',
+    'hip-hop': 'Hip-Hop',
+    'house': 'House',
+    'igbo-gospel-hip-hop': 'Igbo Gospel Hip-Hop',
+    'igbo-highlife': 'Igbo Highlife',
+    'igbo-rap': 'Igbo Rap',
+    'indie-pop': 'Indie-Pop',
+    'jazz': 'Jazz',
+    'neo-soul': 'Neo-Soul',
+    'pop': 'Pop',
+    'r&b': 'R&B',
+    'ragga': 'Ragga',
+    'reggae': 'Reggae',
+    'soca': 'Soca',
+    'soul': 'Soul',
+    'trap': 'Trap',
+    'world': 'World',
+    'yoruba': 'Yoruba'
+}
+
+router.get('/:title', (req, res) => {
+    if(genres[`${req.params.title}`]) res.render('genre', {title: genres[`${req.params.title}`]});
+});
+
 module.exports = router;
