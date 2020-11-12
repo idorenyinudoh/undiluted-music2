f32b1f8b0bbfad228de060e8da4b15eed8628a3a
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-11-12 21:40:00 +0100

    load urls and titles of artistes from db and pass to pug on '/artistes' get

--- routes/artistes.js
+++ routes/artistes.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const artistes = {
-    '2-baba': '2 Baba',
-    '9ice': '9ice',
-    'abstraktt': 'Abstraktt',
-    'adekunle-gold': 'Adekunle Gold',
-    'ajebutter22': 'Ajebutter22',
-    'asa': 'Asa',
-    'ayanfe': 'Ayanfe'
-}
+const db = require('../db');
+
+const urlTitle = {};
+
+async function fetchArtistes() {
+    const collection = db.get().collection('artistes');
+
+    const artistes = await collection.find({},{sort:{title:1},projection:{_id:0}}).toArray();
+
+    artistes.forEach(el => {
+        urlTitle[el.url] = el.title;
+    });
+
+    console.log(urlTitle);
+};
 
 router.get('/artistes', (req, res) => {
-    res.render('artistes');
+    if(Object.keys(urlTitle).length > 0) res.render('artistes', {artistes: urlTitle});
+    else fetchArtistes().then(() => {
+        res.render('artistes', {artistes: urlTitle});
+    });
 });
 router.get('/artistes/:artiste', (req, res) => {
     if(artistes[`${req.params.artiste}`]) res.render('artiste', {artiste: artistes[`${req.params.artiste}`]});
